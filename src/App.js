a52965bae41d28a3188eb6b0887330d4f81b19a5
Git-bot9000 <amandeep09020@gmail.com>
2020-07-29 22:23:01 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Jumbotron} from 'reactstrap';
+import {Image, Button} from 'react-bootstrap'
+import bgimg from './assets/images/Jumbotron1.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="jumbotronContainer">
+        <Image className = "jumbotronImage" src={bgimg} fluid className="img-fluid" />
+        <h2>This is what we do in one Line</h2><Button className="buttonBlack" variant='dark'>Learn More</Button>
+      </div>
     </div>
   );
 }
